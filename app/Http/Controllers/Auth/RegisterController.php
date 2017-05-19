e8c3863595441671ec121721fd0abd85d7f0ace7
Mario Fehr <mario.fehr@gmx.at>
2017-05-19 09:23:46 +0200

    finished Laravel Socialite integration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,4 +68,48 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        try
+        {
+            $socialUser = Socialite::driver($provider)->user();
+        }
+        catch(\Exception $e)
+        {
+            return redirect('/');
+        }
+        //check if we have logged provider
+        $socialProvider = SocialProvider::where('provider_id',$socialUser->getId())->first();
+        if(!$socialProvider)
+        {
+            //create a new user and provider
+            $user = User::firstOrCreate(
+                ['email' => $socialUser->getEmail()],
+                ['name' => $socialUser->getName()]
+            );
+            $user->socialProviders()->create(
+                ['provider_id' => $socialUser->getId(), 'provider' => $provider]
+            );
+        }
+        else
+            $user = $socialProvider->user;
+        auth()->login($user);
+        return redirect('/home');
+    }
+
 }
--- database/migrations/2017_05_19_063639_create_social_providers_table.php
+++ database/migrations/2017_05_19_063639_create_social_providers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePlaylistAPIsTable extends Migration
+class CreateSocialProvidersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreatePlaylistAPIsTable extends Migration
      */
     public function up()
     {
-        Schema::create('playlistapis', function (Blueprint $table) {
+        Schema::create('social_providers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('type');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('provider_id');
+            $table->string('provider');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ class CreatePlaylistAPIsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('playlistapis');
+        Schema::dropIfExists('social_providers');
     }
 }
